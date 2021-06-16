16d4a07e707c7146e5dfbf02e55c488ab4d75102
ik373321 <82790121+ik373321@users.noreply.github.com>
2021-06-16 09:56:56 +0900

    投稿機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,25 @@
 class TweetsController < ApplicationController
+def index
+  @tweet = Tweet.order('created_at DESC')
+end
+def new
+  @tweet = Tweet.new
+end
+def create
+  # binding.pry
+  @tweet = Tweet.new(tweets_params)
+  if @tweet.valid?
+    @tweet.save
+    redirect_to root_path
+  else
+
+    render :new
+  end
+end
+
+
+private
+def tweets_params
+  params.require(:tweet).permit(:memo).merge(user_id: current_user.id)
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_105346) do
+ActiveRecord::Schema.define(version: 2021_06_15_121846) do
+
+  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "memo", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -25,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_06_15_105346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
